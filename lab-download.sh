a73c43df83bcbae2cdfad65a8d48af5468ffd04b
Brennan Wilkes <brennan@codexwilkes.com>
2020-05-14 22:39:45 -0700

    Comments and tweaks

--- lab-download.sh
+++ lab-download.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 #Script for downloading and formatting lab submissions
+
+#Run by using ./lab-download.sh OUTPUT_PATH ZIP_FILE_PATH
+
+#OUTPUT_PATH is the folder you would like to output the submissions to.
+#The script will then create a sub folder, so outputing to ~/Desktop/ is a good choice.
+
+#ZIP_FILE_PATH is the path to the downloaded zip file from d2l brightspace. *NOTE*, do
+#not change the file name of the zip, as it contains meta data required for this script_name
+#to properly format the submissions. The zip will not be deleted, in case of script failure.
+
 #Brennan Wilkes
 
 #Dependency check
@@ -26,6 +36,10 @@ zipfile="$2"
 destination_folder="$1"
 
 lab_number=$( echo -n "$zipfile" | grep -o '^.*Download' | sed 's/ Download//' | sed 's/ /-/' | tr '[:upper:]' '[:lower:]')
+[ "$lab_number" = "" ] || [ "$lab_number" = " " ] && {
+	echo "Invalid zip file name. Please do not rename the zip file. Leave it as downloaded from d2l brightspace"
+	exit 1
+}
 
 #Generate output director
 eval lab_folder=$( echo -n "$destination_folder/$lab_number/" | tr -s '/')
@@ -78,23 +92,32 @@ find . -type f -print | while IFS= read -r  file; do
 		rm "$( ls -t | tail -n1 )"
 	}
 
-	#exit back to main
+	#exit back to main directory
 	cd ..
 done
 
 #Iterate over every student folder
 find . -mindepth 1 -maxdepth 1 -type d -print | while IFS= read -r  student; do
+
+	#Enter student folder
 	cd "$student"
 
+	#Grab submission name and extension type
 	fn=$( ls )
 	extension=$( ls | grep -o '\.[^.]*$' )
 
+	#unzip zip file submissions and remove archive
 	[ "$extension" = ".zip" ] && {
 		unzip -q "$fn"
 		rm "$fn"
 	} || {
+
+		#Rename non-zip files to student name
 		mv "$fn" "$( echo -n "$student" | sed 's/\.\///' )$extension"
 	}
 
+	#Exit to main directory
 	cd ..
 done
+
+exit 0
