8ff93c8b923af4dd55733f6c2123e61fb8beb935
Brennan Wilkes <brennan@codexwilkes.com>
2020-05-14 22:32:41 -0700

    Completed basic cross course zip file splitting

--- lab-download.sh
+++ lab-download.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 #Script for downloading and formatting lab submissions
-
 #Brennan Wilkes
 
 #Dependency check
@@ -14,45 +13,88 @@ unzip -v 2>/dev/null >/dev/null
 	exit 2
 };
 
-[ "$#" -lt 1 ] && {
-	echo "usage: $script_name ZIP_FILE_PATH"
+#Usage message
+script_name=$( echo -n "$0" | grep -o '[^/]*$' )
+[ "$#" -lt 2 ] && {
+	echo "usage: $script_name OUTPUT_PATH ZIP_FILE_PATH"
 	exit 1
 }
 
-rename_entry(){
-	echo "got $1"
-}
+#Parse lab/assignment number/name
+zipfile="$2"
+#destination output folder
+destination_folder="$1"
 
-script_name=$( echo -n "$0" | grep -o '[^/]*$' )
-destination_folder="~/Desktop/cpsc1160-ta/"
-zipfile="$1"
 lab_number=$( echo -n "$zipfile" | grep -o '^.*Download' | sed 's/ Download//' | sed 's/ /-/' | tr '[:upper:]' '[:lower:]')
+
+#Generate output director
 eval lab_folder=$( echo -n "$destination_folder/$lab_number/" | tr -s '/')
 
-mkdir "$lab_folder" 2>/dev/null
+#create output directory tree
+[ -d "$lab_folder" ] || {
+	mkdir "$lab_folder"
+}
+[ -d "${lab_folder}unmarked/" ] || {
+	mkdir "${lab_folder}unmarked/"
+}
+[ -d "${lab_folder}marked/" ] || {
+	mkdir "${lab_folder}marked/"
+}
 
-rm -rf $lab_folder*
+#unzip submissions
+unzip -q "$zipfile" -d "${lab_folder}unmarked/"
 
-unzip -q "$zipfile" -d "$lab_folder"
+#cd to submissions folder
+cd "${lab_folder}unmarked/"
 
-cd "$lab_folder"
-
+#Remove manifest
 rm "index.html"
 
-mv *Pham* "97885-96739 - H'ong Pham - May 12, 2020 744 PM - Lab1-100271972.zip"
-mv *Wesley* "58278-96739 - Wes-ley Ng - May 13, 2020 1056 AM - lab1.zip"
-
-
-#57006-96739 - Erik Dengler - May 6, 2020 231 PM - Lab-One.zip
-#57006-96739\x00Erik Dengler\x00May 6, 2020 231 PM\x00Lab-One.zip
-
+#Iterate over every submission
 find . -type f -print | while IFS= read -r  file; do
+
+	#Parse student name
 	name=$( echo -n "$file" | sed 's/ - /\x00/g' | cut -d '' -f2 | tr ' ' '-' )
+
+	#Parse submission date
 	date=$( echo -n "$file" | sed 's/ - /\x00/g' | cut -d '' -f3 )
+
+	#Parse submission extension
 	extension=$( echo -n "$file" | sed 's/ - /\x00/g' | cut -d '' -f4 | grep -o '\.[^.]*$')
 
-	mkdir "$name" 2>/dev/null
+	#Create named folder
+	[ -d "$name" ] || {
+		mkdir "$name"
+	}
 
+	#Move submission into folder and rename to date
 	mv "$file" "$name/$date$extension"
 
+	#Enter named folder
+	cd "$name"
+
+	#Remove older submissions
+	[ $( ls -1 | wc -l ) -gt 1 ] && {
+		rm "$( ls -t | tail -n1 )"
+	}
+
+	#exit back to main
+	cd ..
+done
+
+#Iterate over every student folder
+find . -mindepth 1 -maxdepth 1 -type d -print | while IFS= read -r  student; do
+	cd "$student"
+
+	fn=$( ls )
+	extension=$( ls | grep -o '\.[^.]*$' )
+
+	[ "$extension" = ".zip" ] && {
+		unzip -q "$fn"
+		rm "$fn"
+	} || {
+		mv "$fn" "$( echo -n "$student" | sed 's/\.\///' )$extension"
+	}
+
+	cd ..
 done
