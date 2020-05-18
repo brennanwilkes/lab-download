ee526e31adef75ef1bdcabf2038dbdef36681f64
Brennan Wilkes <brennan@codexwilkes.com>
2020-05-17 17:17:26 -0700

    Big formatting changes with settings mode. Please check --help for new format

--- README.md
+++ README.md
@@ -3,17 +3,20 @@ A tool to download and format lab/assignment submissions for marking by Brennan 
 
 *first*, download all lab/assignment submissions into a *single* zip file, and do not rename the file.
 
-Then, run by using ./lab-format.sh [-cpp] OUTPUT_PATH ZIP_FILE_PATH
+Then, run by using ./lab-format.sh [ZIP_FILE_PATH] [OUTPUT_PATH]
+
+**Important**
+Usage has recently changed. please run ./lab-format --help for help with settings
 
 **example**
-./lab-format.sh /home/Desktop/ '/home/Downloads/Lab 01 Download May 14, 2020 1129 PM.zip'
-
-OUTPUT_PATH is the folder you would like to output the submissions to.
-The script will then create a sub folder, so outputing to ~/Desktop/ is a good choice.
+./lab-format.sh '/home/Downloads/Lab 01 Download May 14, 2020 1129 PM.zip' /home/Desktop/
 
 ZIP_FILE_PATH is the path to the downloaded zip file from d2l brightspace. *NOTE*, do
 not change the file name of the zip, as it contains meta data required for this script_name
 to properly format the submissions. Don't worry, the zip will not be deleted, in case of script failure.
+
+OUTPUT_PATH is the folder you would like to output the submissions to.
+The script will then create a sub folder, so outputing to ~/Desktop/ is a good choice.
 
 If you are marking .cpp projects, add the -cpp flag, which
 will provide additional C++ support including extra formatting, and auto-compiling.
--- lab-format.sh
+++ lab-format.sh
@@ -2,19 +2,73 @@
 
 #Script for downloading and formatting lab submissions
 
-#Run by using ./lab-format.sh [-cpp] OUTPUT_PATH ZIP_FILE_PATH
+#Run by using ./lab-format.sh [-cpp] [ZIP_FILE_PATH] [OUTPUT_PATH]
 
-#OUTPUT_PATH is the folder you would like to output the submissions to.
-#The script will then create a sub folder, so outputing to ~/Desktop/ is a good choice.
+#**If you do not provide one or both of these arguments, directories from your
+#  default settings folder. Your default settings folder is ~/.lab-format-settings, but
+#  can be changed below**
 
 #ZIP_FILE_PATH is the path to the downloaded zip file from d2l brightspace. *NOTE*, do
 #not change the file name of the zip, as it contains meta data required for this script_name
 #to properly format the submissions. The zip will not be deleted, in case of script failure.
 
+#OUTPUT_PATH is the folder you would like to output the submissions to.
+#The script will then create a sub folder, so outputing to ~/Desktop/ is a good choice.
+
 #If you are a cpsc1160 TA, or are marking .cpp projects in general, add the -cpp flag, which
 #will provide additional C++ support including extra formatting, and auto-compiling.
 
 #Brennan Wilkes
+
+
+#-----------------------------------------------SETUP-----------------------------------------------
+
+
+#Path to lab settings folder. Change this if needed
+LAB_FORMAT_SETTINGS_PATH="~/.lab-format-settings/"
+
+#g++ compile command
+compile_cmd="g++ *.cpp -Wall -g -fsanitize=address -std=c++14 -o main"
+
+#Usage message
+script_name=$( echo -n "$0" | grep -o '[^/]*$' )
+
+#Print all current settings
+print_all_settings() {
+	find "${LAB_FORMAT_SETTINGS_PATH}" -type f -print | while IFS= read -r  setting; do
+		echo "$( echo -n $setting | grep -o '[^/]*$' ) : $( cat $setting )"
+	done
+}
+
+#Reset all settings to defaults
+reset_all_settings() {
+	[ -z "$( ls $LAB_FORMAT_SETTINGS_PATH )" ] || {
+		eval rm "$LAB_FORMAT_SETTINGS_PATH*"
+	}
+	echo -n "~/Desktop" > "${LAB_FORMAT_SETTINGS_PATH}working_directory"
+	echo -n "~/Downloads" > "${LAB_FORMAT_SETTINGS_PATH}zip_search_directory"
+}
+
+#-----------------------------------------------SETTINGS-----------------------------------------------
+
+
+#Update formatting and export
+export LAB_FORMAT_SETTINGS_PATH=$( eval echo -n "$LAB_FORMAT_SETTINGS_PATH/" | tr -s '/')
+
+#Check for settings file
+[ ! -d "$LAB_FORMAT_SETTINGS_PATH" ] && {
+	#Create settings
+	echo "First time run - creating settings folder at $LAB_FORMAT_SETTINGS_PATH"
+	mkdir "$LAB_FORMAT_SETTINGS_PATH"
+	reset_all_settings
+}
+
+#load settings
+working_path=$( eval echo -n $( cat "${LAB_FORMAT_SETTINGS_PATH}working_directory" ) )
+zip_search_directory=$( eval echo -n $( cat "${LAB_FORMAT_SETTINGS_PATH}zip_search_directory" ) )
+
+
+#-----------------------------------------------DEPENDENCY-----------------------------------------------
 
 
 #Dependency check
@@ -27,6 +81,10 @@ unzip -v 2>/dev/null >/dev/null
 	exit 2
 };
 
+
+#-----------------------------------------------MODE FLAGS-----------------------------------------------
+
+
 #C++ mode check
 [ "$1" = "-cpp" ] &&{
 	shift
@@ -35,41 +93,128 @@ unzip -v 2>/dev/null >/dev/null
 	cpp_mode=1
 }
 
-#g++ compile command
-compile_cmd="g++ *.cpp -Wall -g -fsanitize=address -std=c++14 -o main"
-
-
-#Usage message
-script_name=$( echo -n "$0" | grep -o '[^/]*$' )
-[ "$#" -lt 2 ] && {
-	echo "Invalid usage format"
-	echo "usage: $script_name [-cpp] OUTPUT_PATH ZIP_FILE_PATH"
-	exit 1
+[ "$1" = "--help" ] && {
+	echo "usage: $script_name [ZIP_FILE_PATH] [OUTPUT_PATH] \n\n"
+	echo "  Arguments: \n"
+	echo "  [ZIP_FILE_PATH]  Path to zip file downloaded from d2l containing all student submissions \n"
+	echo "  [OUTPUT_PATH]  Directory to output lab folders to. Default is ~/Desktop/ \n\n"
+	echo "  Options: \n"
+	echo "  -cpp  C++ Specific features. Will remove students .o and main files, and will attempt to auto-compile .cpp files \n"
+	echo "  --help Display usage message. Man page coming soon (maybe?)\n"
+	echo "  -s SETTING VALUE  updates a setting, 'SETTING', to value 'VALUE'"
+	echo "  -s RESET  reset to default settings"
+	echo "  -s VIEW  view all current settings\n"
+	exit 0
 }
 
-#Parse lab/assignment number/name
-zipfile="$2"
+#Settings
+[ "$1" = "-s" ] && {
+	shift
+
+	#Reset
+	[ "$1" = "RESET" ] && {
+		reset_all_settings
+		print_all_settings
+		exit 0
+	} || [ "$1" = "VIEW" ] && {
+		#Print
+		print_all_settings
+		exit 0
+	}
+
+	#Check required arguments
+	[ "$#" -eq 2 ] && {
+
+		#Check valid setting key
+		[ -f "${LAB_FORMAT_SETTINGS_PATH}$1" ] && {
+
+			#update setting
+			echo -n "$2" > "${LAB_FORMAT_SETTINGS_PATH}$1"
+			print_all_settings
+
+			exit 0
+		} || {
+
+			#Invalid
+			echo "Invalid setting $1"
+			print_all_settings
+
+			exit 1
+		}
+	} || {
+
+		#Error message
+		echo "Inavlid settings key pair $1 - $2"
+		echo "Usage: $script_name -s SETTING VALUE"
+		echo "Usage: $script_name -s RESET"
+		echo "Usage: $script_name -s VIEW"
+		echo "See $script_name --help for more info"
+		exit 1
+	}
+}
+
+
+#-----------------------------------------------ARGUMENTS-----------------------------------------------
+
+
+#Set explicit zipfile name
+[ "$#" -ge 1 ] && {
+	zipfile="$1"
+	shift
+} || {
+	#Search zipfile directory for newest zip file
+
+	#search for files matching ' Download [Month] [date], [year] [time] AM/PM.zip'
+	zipfile=$( find "$zip_search_directory" -maxdepth 1 -type f -print | grep -E ' Download \w+ [0-9][0-9]?, [0-9][0-9][0-9][0-9] [0-9][0-9]?[0-9][0-9]? [AP]M.zip$' | head -n1 )
+	[ -f "$zipfile" ] && {
+		echo "Autodetected '$( echo -n "$zipfile" | grep -o '[^/]*$' )'"
+		echo -n "Use? [y]/[n]: "
+		read confirmation
+		confirmation=$( echo -n "$confirmation" | tr "[[:upper:]]" "[[:lower:]]" )
+		[ "$confirmation" = "y" ] || [ "$confirmation" = "yes" ] || {
+			echo "Please explicitly state zipfile as an argument"
+			echo "See $script_name --help for more info"
+			exit 1
+		}
+	}
+}
+
+
+
+#Check for valid zipfile
 [ ! -f "$zipfile" ] && {
 	echo "Invalid zipfile name $zipfile"
+	echo "See $script_name --help for more info"
 	exit 1
 }
 
-#destination output folder
-destination_folder="$1"
-[ ! -d "$destination_folder" ] && {
-	echo "Invalid destination folder $destination_folder"
+
+#Set explicit working path
+[ "$#" -ge 1 ] && {
+	working_path="$1"
+	shift
+}
+
+#check for valid working path
+[ ! -d "$working_path" ] && {
+	echo "Invalid destination folder $working_path"
+	echo "See $script_name --help for more info"
 	exit 1
 }
+
+
+#-----------------------------------------------MAIN-----------------------------------------------
 
 
 lab_number=$( echo -n "$zipfile" | grep -o '[^/]*$' | grep -o '^.*Download' | sed 's/ Download//' | sed 's/ /-/' | tr '[:upper:]' '[:lower:]')
 [ "$lab_number" = "" ] || [ "$lab_number" = " " ] && {
 	echo "Invalid zip file name. Please do not rename the zip file. Leave it as downloaded from d2l brightspace"
+	echo "See $script_name --help for more info"
 	exit 1
 }
 
 #Generate output director
-eval lab_folder=$( echo -n "$destination_folder/$lab_number/" | tr -s '/')
+eval lab_folder=$( echo -n "$working_path/$lab_number/" | tr -s '/')
 
 #create output directory tree
 [ -d "$lab_folder" ] || {
@@ -110,6 +255,7 @@ find . -type f -print | while IFS= read -r  file; do
 		#Check for a duplicate submission
 		[ $? -ne 0 ] && {
 			echo "Please delete all existing student submission folders and rerun $script_name"
+			echo "See $script_name --help for more info"
 			exit 1
 		}
 	}
