293810ee5ae5cf067ac1e22c5d36d0f33cd2db1b
Brennan Wilkes <brennan@codexwilkes.com>
2020-05-21 14:13:59 -0700

    version update

--- lab-format.sh
+++ lab-format.sh
@@ -33,7 +33,7 @@ script_name=$( echo -n "$0" | grep -o '[^/]*$' )
 #all possible settings
 settings_list="working_directory zip_search_directory compile_cmd compile_output_name"
 
-export VERSION="1.02"
+export VERSION="1.03"
 
 
 #-----------------------------------------------FUNCTIONS-----------------------------------------------
